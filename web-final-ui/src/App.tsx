7326df67c2345054d6da9e10f5cf3e797ee23f61
ortal124 <ortal120400@gmail.com>
2025-01-21 17:13:27 +0200

    add GoogleOAuthProvider

--- web-final-ui/src/App.tsx
+++ web-final-ui/src/App.tsx
@@ -1,14 +1,18 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './App.css'
 
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
+    <GoogleOAuthProvider
+    clientId='34674964625-qi6of44fa4cm2jpfhtgd9tlu5f0ib370.apps.googleusercontent.com'>
     <AuthProvider>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>    
     </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
