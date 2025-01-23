9f8b294ad2655f22479e6ad45e7cc866472a9d78
ortal124 <ortal120400@gmail.com>
2025-01-23 12:30:41 +0200

    create component folder = delete context

--- web-final-ui/src/components/App.tsx
+++ web-final-ui/src/components/App.tsx
@@ -1,17 +1,14 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import './App.css'
 
-import { AuthProvider } from './context/AuthContext';
+import RegistrationForm from './RegistrationForm';
 
 function App() {
   return (
     <GoogleOAuthProvider
     clientId='34674964625-qi6of44fa4cm2jpfhtgd9tlu5f0ib370.apps.googleusercontent.com'>
-    <AuthProvider>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>    
-    </AuthProvider>
+      <div>
+        <RegistrationForm />
+      </div>
     </GoogleOAuthProvider>
   );
 }
---
+++ web-final-ui/src/components/RegistrationForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const RegistrationForm: FC = () => {
+    return (
+      <p className="read-the-docs">
+        RegistrationForm
+      </p>                     
+    )
+}
+
+export default RegistrationForm
