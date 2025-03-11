7e53c60d6346989cdf29f758eb107b4270fea093
ortal124 <ortal120400@gmail.com>
2025-03-11 22:07:47 +0200

    get posts from server + basic scrolling

--- web-final-ui/src/services/intefaces/post.ts
+++ web-final-ui/src/services/intefaces/post.ts
@@ -1,4 +1,6 @@
 export interface Post {
+    
+    _id?: string;
     text: string;
     image: File;
     userId: string;
@@ -10,4 +12,11 @@ export interface generatedPostText {
     text: {
       quote: string;
     }
+  }
+
+export interface getPostsResponse {
+  downloadedPosts: Post[];
+  totalPosts: number;
+  totalPages: number;
+  currentPage: number;
   }
