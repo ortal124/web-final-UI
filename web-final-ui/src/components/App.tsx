1860091d23e0e4cfac212d91b7b32e55389cf552
ortal124 <ortal120400@gmail.com>
2025-03-18 15:11:01 +0200

    make defailt screen feed when logged in

--- web-final-ui/src/components/App.tsx
+++ web-final-ui/src/components/App.tsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import RegistrationForm from './RegistrationForm';
-import LogInForm from './LoginForm';
-import UserFeed from './UserFeed';
-import UserProfile from './UserProfile';
+import RegistrationForm from "./RegistrationForm";
+import LogInForm from "./LoginForm";
+import UserFeed from "./UserFeed";
+import UserProfile from "./UserProfile";
 import Sidebar from "./SideBar";
-import AddPost from './AddPost';
-import PostDetailPage from './PostDetail';
+import AddPost from "./AddPost";
+import PostDetailPage from "./PostDetail";
 
 import "../styles/App.css";
 import ProtectedRoute from "./auth-utils/ProtectedRoute";
 import { AuthProvider } from "./auth-utils/AuthContext";
+
+const RedirectToDefault: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    navigate(userId ? "/feed" : "/login");
+  }, [navigate]);
+
+  return null;
+};
 
 const App: React.FC = () => {
   return (
@@ -21,9 +32,10 @@ const App: React.FC = () => {
         <Router>
           <div className="app-container">
             <Routes>
+              <Route path="/" element={<RedirectToDefault />} />
               <Route path="/login" element={<LogInForm />} />
               <Route path="/register" element={<RegistrationForm />} />
-              
+
               <Route element={<ProtectedRoute />}>
                 <Route
                   path="/feed"
@@ -58,18 +70,19 @@ const App: React.FC = () => {
                     </div>
                   }
                 />
-                <Route 
-                  path="/post/:postId" 
+                <Route
+                  path="/post/:postId"
                   element={
                     <div className="main-layout">
                       <Sidebar />
                       <div className="content">
-                        <PostDetailPage/>
+                        <PostDetailPage />
                       </div>
                     </div>
-                  }/>
-            </Route>
-            <Route path="*" element={<LogInForm />} />
+                  }
+                />
+              </Route>
+              <Route path="*" element={<RedirectToDefault />} />
             </Routes>
           </div>
         </Router>
